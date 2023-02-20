c6fa87bb1b1cf792fe55ae011e7599e842511e66
sangeetha-ram <sangiammu1020@gmail.com>
2023-02-20 13:39:06 +0530

    Typography

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>
+        <Typography variant='h5' sx={{color:'green'}} >Hello World</Typography>
+    </div>
+  )
+}
